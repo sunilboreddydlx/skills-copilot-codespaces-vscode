efbcb06c50234f350a71fd5b27eeb2f954f791e2
sunilboreddydlx <81694014+sunilboreddydlx@users.noreply.github.com>
2024-07-04 09:36:08 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,44 +1,23 @@
-// create web server
-// npm install express
-// npm install body-parser
-const express = require('express');
-const bodyParser = require('body-parser');
-const fs = require('fs');
+//Create web server with express
+const express = require('express')
+const app = express()
 
-const app = express();
-app.use(bodyParser.json());
-
-// get all comments
+//Create a route to get comments
 app.get('/comments', (req, res) => {
-  fs.readFile('comments.json', (err, data) => {
-    if (err) {
-      res.status(500).send('Error reading comments.json');
-    } else {
-      res.send(data);
-    }
-  });
-});
+    res.json({
+        comments: [
+            {
+                id: 1,
+                body: "Hello world"
+            },
+            {
+                id: 2,
+                body: "Hi, how are you?"
+            }
+        ]
+    })
+})
 
-// add a comment
-app.post('/comments', (req, res) => {
-  fs.readFile('comments.json', (err, data) => {
-    if (err) {
-      res.status(500).send('Error reading comments.json');
-    } else {
-      const comments = JSON.parse(data);
-      comments.push(req.body);
-      fs.writeFile('comments.json', JSON.stringify(comments), (err) => {
-        if (err) {
-          res.status(500).send('Error writing comments.json');
-        } else {
-          res.send('Comment added!');
-        }
-      });
-    }
-  });
-});
-
-// start web server
-app.listen(3000, () => {
-  console.log('Server is running on http://localhost:3000');
-});
+app.listen(3001, () => {
+    console.log('Server is running on http://localhost:3001')
+})
